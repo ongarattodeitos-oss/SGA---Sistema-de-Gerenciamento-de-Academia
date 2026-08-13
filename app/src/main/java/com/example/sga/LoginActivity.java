d4c11494b8a75431ea499fc6f94115eab91a3b6e
ongarattodeitos-oss <ongaratto.deitos@gmail.com>
2026-08-13 13:53:24 -0300

    cader

--- app/src/main/java/com/example/sga/LoginActivity.java
+++ app/src/main/java/com/example/sga/LoginActivity.java
@@ -256,11 +256,6 @@ public class LoginActivity extends AppCompatActivity {
                                     email
                             );
 
-                            intent.putExtra(
-                                    "tipo",
-                                    tipo
-                            );
-
                             startActivity(intent);
 
                             finish();
